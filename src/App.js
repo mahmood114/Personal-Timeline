6b497241293e3c1d9902aa068cc72333056a9330
mahmood114 <mahmoodalmohri@gmail.com>
2021-06-10 16:11:46 +0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components";
 
 function App() {
+  
+  const Title = styled.h1`
+    text-align: center;
+  `;
+  const Description = styled.p`
+    text-align: center;
+    
+  `;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Title>Sayed Mahmood's life... basically</Title>
+      <Description>My name is Sayed Mahmood Almohri. I was born in Nov 22nd, 1998.</Description>
+      <Description>I got into Kuwait University after I graduated from high school in 2016. I got into the college of Engineering where I studied Computer Engineering.</Description>
     </div>
   );
 }
